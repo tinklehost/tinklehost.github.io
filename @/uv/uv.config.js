7c55e58cb63de2bb34c8cd015d166254e6daacfa
tinklehost <strangerthingfanman@gmail.com>
2025-12-13 07:04:16 -0500

    Add files via upload

---
+++ @/uv/uv.config.js
@@ -0,0 +1,12 @@
+self.__uv$config = {
+    prefix: '/@/0c2f1ddc-5748-436f-ae7c-89b5f708d90e/',
+    bare: 'https://interstellar.flamepass.com/ca/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/@/uv/uv.handler.js',
+    bundle: '/@/uv/uv.bundle.js',
+    config: '/@/uv/uv.config.js',
+    sw: '/@/uv/uv.sw.js',
+};
+
+
